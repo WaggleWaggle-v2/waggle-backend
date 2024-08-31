3ca7be52a10a168836f6371a2a656b695d8a15ae
Sfer7 <forever_kaka@naver.com>
2024-08-31 16:45:10 +0900

    Fix: Google Login Method 변경

--- modules_application/application_member/src/main/java/unius/application_member/controller/AuthController.java
+++ modules_application/application_member/src/main/java/unius/application_member/controller/AuthController.java
@@ -3,7 +3,7 @@ package unius.application_member.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import unius.application_member.service.AuthService;
@@ -15,7 +15,7 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login/google")
+    @GetMapping("/login/google")
     public ResponseEntity<String> googleLogin(
             @RequestParam("code") String code) {
         return ResponseEntity.ok(authService.googleLogin(code));
