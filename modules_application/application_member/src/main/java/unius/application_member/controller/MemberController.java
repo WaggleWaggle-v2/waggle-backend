0e891f2c67dcc2d20155d44e23b9ee60418e3a5d
Sfer7 <forever_kaka@naver.com>
2024-09-06 15:14:23 +0900

    Fix: getMyUserInfo Dto 제네릭 지정

--- modules_application/application_member/src/main/java/unius/application_member/controller/MemberController.java
+++ modules_application/application_member/src/main/java/unius/application_member/controller/MemberController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import unius.application_member.dto.GetMyUserInfoDto;
 import unius.application_member.service.MemberService;
 
 @Controller
@@ -16,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/info/me")
-    public ResponseEntity<?> getMyUserInfo(
+    public ResponseEntity<GetMyUserInfoDto.Response> getMyUserInfo(
             @RequestHeader("X-User-Id-Header") String id) {
         return ResponseEntity.ok(memberService.getMyUserInfo(Long.parseLong(id)));
     }
