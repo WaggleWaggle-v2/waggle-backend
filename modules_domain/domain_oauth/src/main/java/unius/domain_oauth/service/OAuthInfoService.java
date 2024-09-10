8cc9611b2ec96345948720f31b90ed1073363338
Sfer7 <forever_kaka@naver.com>
2024-09-11 03:12:49 +0900

    Fix: 영속성 컨텍스트 오류 수정

--- modules_domain/domain_oauth/src/main/java/unius/domain_oauth/service/OAuthInfoService.java
+++ modules_domain/domain_oauth/src/main/java/unius/domain_oauth/service/OAuthInfoService.java
@@ -1,5 +1,6 @@
 package unius.domain_oauth.service;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import unius.domain_oauth.domain.OAuthInfo;
@@ -13,6 +14,8 @@ import unius.domain_user.service.UserService;
 @RequiredArgsConstructor
 public class OAuthInfoService {
 
+    private final EntityManager entityManager;
+
     private final OAuthInfoRepository oAuthInfoRepository;
     private final OAuthInfoRepositoryQuerydsl oAuthInfoRepositoryQuerydsl;
 
@@ -25,10 +28,12 @@ public class OAuthInfoService {
         if(oAuthInfo == null) {
             user = userService.create();
 
+            User proxyUser = entityManager.find(User.class, user.getId());
+
             oAuthInfo = OAuthInfo.builder()
                     .oauthId(oAuthId)
                     .platform(platform)
-                    .user(user)
+                    .user(proxyUser)
                     .build();
 
             oAuthInfoRepository.save(oAuthInfo);
