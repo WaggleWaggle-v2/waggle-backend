c532a525f4d4262cd90ac433e5616c9895245bbd
Sfer7 <forever_kaka@naver.com>
2024-09-14 02:26:16 +0900

    Fix: OAuthToken JsonProperty 명시적 작성

--- modules_system/system_oauth/src/main/java/unius/system_oauth/dto/OAuthTokenDto.java
+++ modules_system/system_oauth/src/main/java/unius/system_oauth/dto/OAuthTokenDto.java
@@ -1,9 +1,12 @@
 package unius.system_oauth.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OAuthTokenDto {
+
+    @JsonProperty("access_token")
     private String access_token;
 
     public void setAccessToken(String accessToken) {
